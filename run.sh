c587ac843bf69c88cc853ac6635a3c6d375d7d96
Chris Augier <chris.augier@gmail.com>
2014-08-01 11:16:35 +0100

    Adding run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
-mkdir -p $WERCKER_CACHE_DIR/wercker/npm
-npm config set cache $WERCKER_CACHE_DIR/wercker/npm
-npm install $WERCKER_NPM_INSTALL_OPTIONS
+# Add GPG public key
+wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
+
+# Select Oracle jazz
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+
+# Seen Oracle Jazz
+echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+
+#add apt repositories
+add-apt-repository deb http://packages.elasticsearch.org/elasticsearch/1.0/debian stable main
+
