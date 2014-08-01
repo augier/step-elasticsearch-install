52ccc36e8d7e5b8f82159058d605d36730d1fb62
Chris Augier <chris.augier@gmail.com>
2014-08-01 13:10:07 +0100

    Removed apt-get updates

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
-# Add GPG public key
-wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
-
-# Select Oracle jazz
-echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-
-# Seen Oracle Jazz
-echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
-
-#add apt repositories
-add-apt-repository deb http://packages.elasticsearch.org/elasticsearch/1.0/debian stable main
-
+sudo apt-get update
+sudo apt-get upgrade
+sudo wget -O /tmp/elasticsearch-0.90.2.deb https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.2.deb
+sudo dpkg -i /tmp/elasticsearch-0.90.2.deb
