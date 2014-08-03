c54030dad71b5adb078541a713c0b9afc5452ff7
Chris Augier <chris.augier@gmail.com>
2014-08-03 20:10:36 +0100

    Removed step

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ if [ ! -n "$WERCKER_ELASTICSEARCH_INSTALL_VERSION" ]; then
   exit 1
 fi
 
-sudo apt-get install openjdk-7-jre-headless
+#sudo apt-get install openjdk-7-jre-headless
 sudo wget -O /tmp/elasticsearch-$WERCKER_ELASTICSEARCH_INSTALL_VERSION.deb https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$WERCKER_ELASTICSEARCH_INSTALL_VERSION.deb
 sudo dpkg -i /tmp/elasticsearch-$WERCKER_ELASTICSEARCH_INSTALL_VERSION.deb
 sudo sed -i 's/# http.enabled: false/http.enabled: true/g' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's/# network.host: 192.168.0.1/network.host: $$HOST$$/g' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/# network.host: 192.168.0.1/network.host: localhost/g' /etc/elasticsearch/elasticsearch.yml
 sudo service elasticsearch restart
